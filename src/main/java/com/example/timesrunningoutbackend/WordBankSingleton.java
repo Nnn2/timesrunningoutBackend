02369f4866d3040da117cfcdde3c762865c37c9a
Nnn2 <nnn2@hi.is>
2022-04-20 17:54:05 +0000

    getNwords
    trying to make the method to get n random words

--- src/main/java/com/example/timesrunningoutbackend/WordBankSingleton.java
+++ src/main/java/com/example/timesrunningoutbackend/WordBankSingleton.java
@@ -1,27 +1,32 @@
 package com.example.timesrunningoutbackend;
+import org.thymeleaf.util.ArrayUtils;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WordBankSingleton {
 
     private static WordBankSingleton instance;
-    private List<Word> wordBank;
+    private static List<Word> wordBank;
+    private static int lastID;
 
     public static WordBankSingleton getInstance() {
         if (instance == null) {
             instance = new WordBankSingleton();
-            instance.wordBank = new ArrayList<>();
+            wordBank = new ArrayList<>();
 
             // String array containing 4 words.
             String[] words = {
                     "Butterfly", "cup", "tea", "fire"
             };
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < words.length; i++) {
                 int id = i + 1;
                 String wordText = words[i];
                 Word word = new Word(id, wordText);
-                instance.wordBank.add(word);
+                wordBank.add(word);
+                lastID = i;
             }
         }
         return instance;
@@ -34,12 +39,33 @@ public class WordBankSingleton {
         return wordBank;
     }
 
-    public void addWord(Word word) {
-        wordBank.add(word);
-    }
-
     //getWords(int n) random number generator between 0 and length of wordbank,
     //add it to list of int in a while loop going n times (check that there are no duplicates of indices)
     //make a list in another loop from the indices in the previous list
-    
+    public List<Word> getNwords(int n) {
+        int i = 0;
+        int[] indices = new int[n];
+        Arrays.fill(indices, -1);
+        List<Word> words = new ArrayList<>();
+        while(i < n) {
+            int index = -1;
+            while(contains(indices,index)) {
+                index = (int) ((Math.random() * (wordBank.size())));
+            }
+            indices[i] = index;
+            words.add(wordBank.get(index));
+            i++;
+        }
+        return words;
+    }
+
+    private static boolean contains(final int[] arr, final int key) {
+        return Arrays.stream(arr).anyMatch(i -> i == key);
+    }
+
+    public static void addWord(String wordString) {
+        Word word = new Word(lastID+1, wordString);
+        wordBank.add(word);
+    }
+
 }
--- src/main/java/com/example/timesrunningoutbackend/WordRestController.java
+++ src/main/java/com/example/timesrunningoutbackend/WordRestController.java
@@ -12,13 +12,26 @@ public class WordRestController {
         return wordBank.getWordBank();
     }
 
-    //TODO: getWords(int n)
+    @RequestMapping("/nWords/{n}")
+    public List<Word> getNWords(@PathVariable(value="n") int n){
+        WordBankSingleton wordBank = WordBankSingleton.getInstance();
+        return wordBank.getNwords(n);
+    }
 
-    @RequestMapping("/words/{Id}")
+    @PostMapping("/addWords")
+    public List<Word> addWords(List<String> words){
+        WordBankSingleton wordBank = WordBankSingleton.getInstance();
+        for (int i = 0; i < words.size(); i++) {
+            WordBankSingleton.addWord(words.get(i));
+        }
+        return null;
+    }
+
+    //change this method to get one word for the tiebreaker (use Word object throughout app to make sure we're not taking a word already part of game...?)
+    @RequestMapping("/word/{Id}")
     public Word getAttr(@PathVariable(value="Id") int id) {
         WordBankSingleton wordBank = WordBankSingleton.getInstance();
         return wordBank.getWordBank().get(id-1);
     }
 
-    //TODO: POST request takes in a list of words and saves them to the signleton with the addWord method
 }
